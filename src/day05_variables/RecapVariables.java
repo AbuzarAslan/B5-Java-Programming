cbdb7bbe39abd19fbf3ef87e8ff2dd9387ba7818
abuza <abuzar.aslanov@gmail.com>
2025-05-31 14:10:39 -0400

    Initial commit

---
+++ src/day05_variables/RecapVariables.java
@@ -0,0 +1,18 @@
+package day05_variables;
+
+public class RecapVariables {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        //declare and assign directly
+        // = --- Assignment Operator
+        // . --- Dor Operator
+        //datatype varName = value;
+        double temp = 25.5;
+        float earthDiameter = 1235824.45f;
+
+        System.out.println("Today temperature was: " + temp + "degrees Fahrenheit");
+    }
+}
---
+++ src/day08_scanner_logical_operators/FirstScanner.java
@@ -0,0 +1,15 @@
+package day08_scanner_logical_operators;
+
+import java.util.Scanner;
+
+public class FirstScanner {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your age:");
+        String myAge = sc.nextLine();
+
+
+    }
+}
